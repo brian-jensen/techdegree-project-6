118e97d42b0219342429db8c530a84bac528aae7
Brian Jensen <31598338+brian-jensen@users.noreply.github.com>
2017-12-13 15:57:22 -0700

    init commit - built framework and added mediaelement.js plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+# techdegree-project-6
+
+Brian Jensen's Interactive Video Player - FETD Project 6
---
+++ js/app.js
@@ -0,0 +1,5 @@
+// mediaplayer.js overides
+$('#mediaplayer').mediaelementplayer({
+  stretching: 'responsive',
+  features: ['playpause', 'current', 'progress', 'duration', 'volume', 'fullscreen'],
+});
